257d36a0ecc3ad0d3a62a74d1fecdffed60f092e
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-02-02 14:26:01 -0600

    Add CORS.  Add documentation.  Do some cleanup. (#27)
    * Add CORS.  Add documentation.  Do some cleanup.
    
    * changes in response to review

--- manifest/manifest.mjs
+++ manifest/manifest.mjs
@@ -5,19 +5,21 @@ import * as utils from "../utilities/shared.mjs"
 export async function findTheManifestByID(id=null){
    let manifest = null
    if(!utils.validateProjectID(id)) return manifest
+
+   // Mock a pause for endpoints that fail, to mock the time it takes for some async stuff to decide it failed.
    const mockPause = new Promise((resolve, reject) => {
      setTimeout(() => {
        resolve(null)
-     }, 3500)
+     }, 1500)
    })
-   if(id){
-      if(id && id===7085) {
-         manifest = await fetch("https://t-pen.org/TPEN/manifest/7085").then(resp => resp.json()).catch(err => {
-            console.error(err)
-            return null
-         })
-      }
+
+   if(id && id===7085) {
+      manifest = await fetch("https://t-pen.org/TPEN/manifest/7085").then(resp => resp.json()).catch(err => {
+         console.error(err)
+         return null
+      })
    }
+   
    // Mock the scenario where it takes a couple seconds to look for but not find the Manifest.
    if(manifest === null){
       manifest = mockPause.then(val => {return null})
