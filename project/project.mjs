db94258f8c563dafb5b3782005cdc47d12f1fe9b
Leandru Martin <leandru.martin@slu.edu>
2024-02-05 10:19:22 -0600

    /project sends local dummy JSON data

--- project/project.mjs
+++ project/project.mjs
@@ -1,6 +1,7 @@
 /** Logic for the /project endpoint */
 
 import * as utils from '../utilities/shared.mjs'
+import * as fs from 'fs'
 
 export async function findTheProjectByID(id = null) {
   let project = null
@@ -14,12 +15,13 @@ export async function findTheProjectByID(id = null) {
   })
 
   if (id && id === 7085) {
-    project = await fetch('https://t-pen.org/TPEN/manifest/7085')
-      .then((resp) => resp.json())
-      .catch((err) => {
+    let projectFileBuffer = fs.readFileSync('./public/project.json', (err, data) => {
+      if (err) {
         console.error(err)
         return null
-      })
+      }
+    })
+    project = projectFileBuffer !== null && JSON.parse(projectFileBuffer.toString())
   }
 
   // Mock the scenario where it takes a couple seconds to look for but not find the Project.
