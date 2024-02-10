ae3cce276720106dbcdaffa3fc3c14d9d9d2a01f
Patrick Cuba <cubap@slu.edu>
2024-02-09 23:39:53 -0600

    Revert "failing tests set up"
    This reverts commit e1b509c85fe6405a2f32a90d401aec4fb8b5b4e8.

--- modules/Line/__tests__/exists.test.mjs
+++ modules/Line/__tests__/exists.test.mjs
@@ -1,21 +1,13 @@
-import { Line } from "../Line.mjs"
+import '../Line.mjs'
 
 describe('Line Class looks how we expect it to. #Line_exists_unit', () => {
   it('Imports Line', () => {
-    expect(Line.constructor).toBeInstanceOf(Function)
+    expect(Line).toBeDefined()
   })
 
   const line = new Line()
 
-  it('has useful methods', () => {
-    expect(line.asJSON).toBeInstanceOf(Function)
-    expect(line.create).toBeInstanceOf(Function)
-    expect(line.delete).toBeInstanceOf(Function)
-    expect(line.save).toBeInstanceOf(Function)
-    expect(line.fetch).toBeInstanceOf(Function)
-  })
-
-  it('configures a correct Annotation', () => {
+  it('generates a correct new Line', () => {
     expect(line.id).toBeDefined()
     const json = line.asJSON()
     expect(json).toBeInstanceOf(Object)
@@ -23,5 +15,12 @@ describe('Line Class looks how we expect it to. #Line_exists_unit', () => {
     expect(json['@context'].toBe("http://www.w3.org/ns/anno.jsonld"))
     expect(json.body).toBeDefined()
     expect(json.target).toBeDefined()
+  })
+
+  it('has useful methods',()=>{
+    expect(line.asJSON).toBeInstanceOf(Function)
+    expect(line.create).toBeInstanceOf(Function)
+    expect(line.delete).toBeInstanceOf(Function)
+    expect(line.update).toBeInstanceOf(Function)
   })
 })
