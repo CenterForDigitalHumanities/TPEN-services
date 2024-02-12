39e430c9795d4556ebfb2ddf3bc156df9bcebf31
Sandeep Kumar Sutharapu <118848198+Sandeep-1-Kumar@users.noreply.github.com>
2024-02-12 17:03:22 -0600

    WIP on issue 19 (#32)
    * added initial code for issue19 page/id
    
    * removed the test cases package in page/tests-not required now
    
    * updated on the previous comments on issue

---
+++ page/__tests__/exists_unit.test.mjs
@@ -0,0 +1,16 @@
+//need to import app for coverage, not for actual testing tho.
+import app from '../../app.mjs'
+
+describe('page endpoint availability unit test (via a check on the app routes). #exists_unit', () => {
+  it('responds to /page/id', () => {
+    let exists = false
+    const stack = app._router.stack
+    for(const middleware of stack){
+      if(middleware.regexp && middleware.regexp.toString().includes("/page")) {
+         exists = true
+         break
+       } 
+    }
+    expect(exists).toBe(true)
+  })
+})
