f04b2347c0cf8e0f4601428189f0cf0e19b5a74e
Leandru Martin <122942864+leandrumartin@users.noreply.github.com>
2024-02-12 17:06:05 -0600

    WIP on Issue 2 (#34)
    * Initial commit on Issue #2, largely duplicating /manifest code to new /project directory
    
    * /project sends local dummy JSON data
    
    * Added tests to project directory
    
    * Addressed comment on mockPause() in project.mjs
    
    ---------
    
    Co-authored-by: Bryan Haberberger <bryan.j.haberberger@slu.edu>

---
+++ project/__tests__/exists_unit.test.mjs
@@ -0,0 +1,16 @@
+//need to import app for coverage, not for actual testing tho.
+import app from '../../app.mjs'
+
+describe('Project endpoint availability unit test (via a check on the app routes). #exists_unit', () => {
+  it('responds to /project/id', () => {
+    let exists = false
+    const stack = app._router.stack
+    for(const middleware of stack){
+      if(middleware.regexp && middleware.regexp.toString().includes("/project")) {
+         exists = true
+         break
+       } 
+    }
+    expect(exists).toBe(true)
+  })
+})
