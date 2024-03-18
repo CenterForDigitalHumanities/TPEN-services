e3c038ded9f0c723413e702faca7dbd8af68f645
BhanuKandula5030 <k.bhannuprasad@gmail.com>
2024-03-18 13:49:03 -0500

    Updated the code to resolve the illegal return error

--- line/line.mjs
+++ line/line.mjs
@@ -10,82 +10,7 @@ export async function findLineById(id = null, options = {}) {
   if (!utils.validateID(id)) {
     return line;
   }
-  const lineFromDB = mockGetByID(id);
 
-  if (lineFromDB !== null) {
-    if (options.text === 'blob') {
-      return { text: lineFromDB.textualBody };
-    } else if (options.image === 'full') {
-      return { image: lineFromDB.canvas };
-    } else if (options.image === 'line') {
-      return { image: `some line image URL for id ${id}` };
-    } else if (options.lookup) {
-      return { relatedDocument: `some ${options.lookup} document for id ${id}` };
-    } else {
-      const jsonResponse = {
-        '@context': lineFromDB['@context'],
-        id: lineFromDB.id,
-        '@type': lineFromDB['@type'],
-        creator: lineFromDB.creator,
-        textualBody: lineFromDB.textualBody,
-        project: lineFromDB.project,
-        canvas: lineFromDB.canvas,
-        layer: lineFromDB.layer,
-        viewer: lineFromDB.viewer,
-        license: lineFromDB.license
-      };
-
-      if (options.view === 'xml') {
-        return generateXML(lineFromDB);
-      } else if (options.view === 'html') {
-        return generateHTML(lineFromDB);
-      }
-      if (options.embed === true) {
-        return { expandedDocument: jsonResponse };
-      }
-
-      return jsonResponse;
-    }
-  }
-
-  return line;
-}
-
-function mockGetByID(id) {
-  if (id !== 123) {
-    return null;
-  }
-  return {
-    id: `#${id}`,
-    '@context': 'http://t-pen.org/3/context.json',
-    '@type': 'Annotation',
-    creator: 'https://store.rerum.io/v1/id/hash',
-    textualBody: `content of annotation ${id}`,
-    project: '#ProjectId',
-    canvas: 'https://example.com/canvas.json',
-    layer: '#AnnotationCollectionId',
-    viewer: `https://static.t-pen.org/#ProjectId/#PageId/#LineId${id}`,
-    license: 'CC-BY'
-  };
-}
-
-function generateXML(lineData) {
-  // Generate XML representation of the line data
-  return `<line><id>${lineData.id}</id><text>${lineData.textualBody}</text></line>`;
-}
-
-function generateHTML(lineData) {
-  // Generate HTML viewer document
-  return `
-    <html>
-      <head><title>Line Viewer</title></head>
-      <body>
-        <h1>Line ${lineData.id} Viewer</h1>
-        <p>${lineData.textualBody}</p>
-      </body>
-    </html>
-  `;
-}
   const mockPause = new Promise((resolve) => {
     setTimeout(() => {
       resolve(null);
@@ -102,5 +27,59 @@ function generateHTML(lineData) {
     line = await mockPause;
   }
 
+  if (line !== null) {
+    if (options.text === 'blob') {
+      return { text: line.textualBody };
+    } else if (options.image === 'full') {
+      return { image: line.canvas };
+    } else if (options.image === 'line') {
+      return { image: `some line image URL for id ${id}` };
+    } else if (options.lookup) {
+      return { relatedDocument: `some ${options.lookup} document for id ${id}` };
+    } else {
+      const jsonResponse = {
+        '@context': line['@context'],
+        id: line.id,
+        '@type': line['@type'],
+        creator: line.creator,
+        textualBody: line.text,
+        project: line.project,
+        canvas: line.canvas,
+        layer: line.layer,
+        viewer: line.viewer,
+        license: line.license
+      };
+
+      if (options.view === 'xml') {
+        return generateXML(line);
+      } else if (options.view === 'html') {
+        return generateHTML(line);
+      }
+      if (options.embed === true) {
+        return { expandedDocument: jsonResponse };
+      }
+
+      return jsonResponse;
+    }
+  }
+
   return line;
+}
+
+function generateXML(lineData) {
+  // Generate XML representation of the line data
+  return `<line><id>${lineData.id}</id><text>${lineData.text}</text></line>`;
+}
+
+function generateHTML(lineData) {
+  // Generate HTML viewer document
+  return `
+    <html>
+      <head><title>Line Viewer</title></head>
+      <body>
+        <h1>Line ${lineData.id} Viewer</h1>
+        <p>${lineData.text}</p>
+      </body>
+    </html>
+  `;
 }
