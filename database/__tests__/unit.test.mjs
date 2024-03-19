30493e635f7385c0166f92a409e7599db8980cab
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-03-19 11:54:24 -0500

    line up unit test stubs

---
+++ database/__tests__/unit.test.mjs
@@ -0,0 +1,5 @@
+describe('A POLY stub test that is always true.  #exists_unit #db',()=>{
+    it('POLY says hello', async () => {
+        expect(true).toBeTruthy()
+    })
+})
--- database/mongo/__tests__/unit.test.mjs
+++ database/mongo/__tests__/unit.test.mjs
@@ -1,4 +1,4 @@
-import * as MongoController from '../index.mjs'
+import MongoController from '../index.mjs'
 
 beforeAll(async () => {
     // None if this will run without a controller that is connected.
@@ -28,43 +28,3 @@ describe('Mongo Database Unit Functions. #db',()=>{
         expect(true).toBeTruthy()
     })
 })
-
-/**
- * THESE BELONG SOMEWHERE ELSE
- */ 
-
-// describe('Project collection methods #functions_unit #db',()=>{
-//     it('creates a new project', async () => {
-//         const project = { name: 'Test Project', description: 'This is a test project.' }
-//         const result = await dbController.createProject(project)
-//         expect(result["@id"]).toBeTruthy()
-//     })
-    
-//     it('assigns a new tool to the project', async () => {
-//         const projectId = 'your-project-id'
-//         const tool = { name: 'Test Tool', version: '1.0.0' }
-//         const result = await dbController.assignToolToProject(projectId, tool)
-//         expect(result.acknowledged).toBe(true)
-//     })
-    
-//     it('grants a group access to the project', async () => {
-//         const projectId = 'your-project-id'
-//         const groupId = 'your-group-id'
-//         const result = await dbController.grantGroupAccessToProject(projectId, groupId)
-//         expect(result.acknowledged).toBe(true)
-//     })
-// })
-
-// describe('Project methods exist #exists_unit',()=>{
-//     it('createProject method exists', () => {
-//         expect(dbController.createProject).toBeInstanceOf(Function)
-//     })
-//     it('assignToolToProject method exists', () => {
-//         expect(dbController.assignToolToProject).toBeInstanceOf(Function)
-//     })
-//     it('grantGroupAccessToProject method exists', () => {
-//         expect(dbController.grantGroupAccessToProject).toBeInstanceOf(Function)
-//     })
-// })
-
-// Add more tests for the remaining methods...
