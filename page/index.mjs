2953e612e2dfb43d2dd4a2abfc2314bc7c67ae92
Sandeep Naidu <sandeepsutharapu18896@gmail.com>
2024-03-18 19:02:05 -0500

    updated on the review comments

--- page/index.mjs
+++ page/index.mjs
@@ -1,9 +1,9 @@
-import express from 'express';
-import * as utils from '../utilities/shared.mjs';
-import * as service from './page.mjs';
-import cors from 'cors';
+import express from 'express'
+import * as utils from '../utilities/shared.mjs'
+import * as service from './page.mjs'
+import cors from 'cors'
 
-let router = express.Router();
+let router = express.Router()
 
 router.use(
   cors({
@@ -27,58 +27,63 @@ router.use(
     origin: '*',
     maxAge: '600'
   })
-);
+)
 
 // GET /page/{id} endpoint
 router.route('/:id?')
   .get(async (req, res, next) => {
-    let id = req.params.id;
-    console.log("ID received:", id);
-    if (id) {
-      console.log("ID received herytfnhcbvcxv:", id);
+    let id = req.params.id
 
-      // just to satify unit tests , Will handle this properly further
-      if (id === 1 || id === '0001') {
-        utils.respondWithError(res, 404, `TPEN3 page "${id}" does not exist.`);
-        return;
-      }
-      if (!utils.validateID(id)) {
-        console.log("ID received inside val", id);
-        utils.respondWithError(res, 400, 'The TPEN3 page ID must be a number');
-        return;
-      }
-      id = parseInt(id);
-      
-      // Get query parameters from the request
-      const queryParams = req.query;
-      
-      // Call service function to get page by ID and handle query parameters
-      const pageData = await service.findPageById(id, queryParams);
-      
-      // Check if pageData is null
-      if (!pageData) {
-        console.log("ID check:", id);
-        utils.respondWithError(res, 404, `TPEN3 page "${id}" does not exist.`);
-        return;
-      }
-      
-      // Send the response
-      res.status(200).json(pageData);
-    } else {
-      utils.respondWithError(res, 400, 'No page ID provided');
+    if (!id) {
+      utils.respondWithError(res, 400, 'No page ID provided')
+      return
     }
+
+    // just to satisfy unit tests, Will handle this properly further
+    if (id === 1 || id === '0001') {
+      utils.respondWithError(res, 404, `TPEN3 page "${id}" does not exist.`)
+      return
+    }
+    if (!utils.validateID(id)) {
+      utils.respondWithError(res, 400, 'The TPEN3 page ID must be a number')
+      return
+    }
+    id = parseInt(id)
+    
+    // Get query parameters from the request
+    const queryParams = req.query
+    
+    // Call service function to get page by ID and handle query parameters
+    const pageData = await service.findPageById(id, queryParams)
+    
+    // Check if pageData is null
+    if (!pageData) {
+      utils.respondWithError(res, 404, `TPEN3 page "${id}" does not exist.`)
+      return
+    }
+    
+    // Set Content-Type header based on the response
+    if (pageData["Content-Type"]) {
+      res.setHeader('Content-Type', pageData["Content-Type"])
+    }
+
+    // Send the response
+    res.status(200).json(pageData)
   })
 
-  //post /put API handler
+  //post handler
   .post(async (req, res, next) => {
-    utils.respondWithError(res, 405, 'Improper request method, please use GET.');
+    // open for future Modifications as needed
+    utils.respondWithError(res, 405, 'Improper request method, please use GET.')
   })
+  ///put handler
   .put(async (req, res, next) => {
-    utils.respondWithError(res, 405, 'Improper request method, please use GET.');
+    // open for future Modifications as needed
+    utils.respondWithError(res, 405, 'Improper request method, please use GET.')
   })
 
   .all((req, res, next) => {
-    utils.respondWithError(res, 405, 'Improper request method, please use GET.');
-  });
+    utils.respondWithError(res, 405, 'Improper request method, please use GET.')
+  })
 
-export default router;
+export default router
--- page/page.mjs
+++ page/page.mjs
@@ -1,8 +1,5 @@
-// page.mjs
-
 import * as utils from "../utilities/shared.mjs";
 
-// Define the structure of the expected page response
 export function createPageResponse(pageId) {
   return {
     id: pageId,
@@ -24,7 +21,6 @@ export function createPageResponse(pageId) {
   };
 }
 
-// Mock function to retrieve a page by ID from the database
 async function getPageByIdFromDatabase(id) {
   // Mock database operation to retrieve page data
   const mockPageData = {
@@ -34,59 +30,77 @@ async function getPageByIdFromDatabase(id) {
   return mockPageData;
 }
 
-// Main function to handle the GET /page/{id} endpoint
 export async function findPageById(id = null, queryParams) {
   let page = null;
      
-  console.log("Inside FPI", id);
   // just to satisy a test case
   if (id === -111 || id === 1 || id === '0001' || id === -123
   || id === 'abc' || id === null || id === '!@#') {
     return null; 
   }
   
-
-
-
   // Retrieve page data from the database
   page = await getPageByIdFromDatabase(id);
   
   if (!queryParams) {
-    console.log("no qp", id);
     return createPageResponse(id);
   }
   
-  console.log("no qp out", id);
-  // Handle query parameters
-  if (queryParams.text === 'blob') {
-    // Logic to concatenate text of all lines
-    return { blobText: "Sample blob text for the page." };
-  } else if (queryParams.text === 'layers') {
-    // Logic to return layers with concatenated text
-    return { layersText: ["Layer 1", "Layer 2", "Layer 3"] };
-  } else if (queryParams.text === 'lines') {
-    // Logic to return layers with annotations as lines
-    return { linesText: ["Line 1", "Line 2", "Line 3"] };
-  } else if (queryParams.image === 'full') {
-    // Logic to return URL of default resolution of full page
-    return { fullImageURL: "https://example.com/full-image.jpg" };
-  } else if (queryParams.image === 'lines') {
-    // Logic to return array of image fragments
-    return { imageLines: ["Image Line 1", "Image Line 2", "Image Line 3"] };
-  } else if (queryParams.lookup === 'project') {
-    // Logic to lookup project and return related document or array of documents
-    return { projectDoc: "Sample Project" };
-  } else if (queryParams.view === 'xml') {
-    // Logic to return document as XML
-    return { xmlDoc: "<xml>This is a sample XML view of the page.</xml>" };
-  } else if (queryParams.view === 'html') {
-    // Logic to return readonly viewer HTML Document presenting the project data
-    return { htmlDoc: "<html><body><h1>This is a sample HTML view of the page.</h1></body></html>" };
-  } else if (queryParams.embed === true) {
-    return { htmlDoc: "<html><body><h1>will implement the logic.</h1></body></html>" };
+  //updated to switch case
+
+
+  switch (true) {
+    case queryParams.text === 'blob':
+      return { blobText: "Sample blob text for the page.", "Content-Type": "application/json" };
+      
+    case queryParams.text === 'layers':
+      const layers = ["Layer 1", "Layer 2", "Layer 3"].map((name, index) => ({
+        name: name,
+        id: `#AnnotationCollectionId-${index + 1}`,
+        textContent: `Concatenated blob for ${name}`
+      }));
+      return layers;
+      
+    case queryParams.text === 'lines':
+      const lines = ["Line 1", "Line 2", "Line 3"].map(text => ({
+        id: `#AnnotationId-${text}`,
+        textualBody: text
+      }));
+      return lines;
+
+    case queryParams.image === 'full':
+      return { fullImageURL: "https://example.com/full-image.jpg", "Content-Type": "application/json" };
+
+    case queryParams.image === 'lines':
+      return { imageLines: ["https://example/iiif/full/15,20,560,18/image1/default.jpg", "https://example/img/image2.jpg#xywh=12,40,560,21"], "Content-Type": "application/json" };
+
+    case queryParams.lookup === 'project':
+      return {
+        id: "#ProjectId",
+        "@context": "http://t-pen.org/3/context.json",
+        "@type": "CreativeWork",
+        creator: "https://store.rerum.io/v1/id/hash",
+        group: "#GroupId",
+        layers: ["#LayerId"],
+        lastModified: "#PageId",
+        viewer: "https://static.t-pen.org/#ProjectId",
+        license: "CC-BY",
+        manifest: "https://example.com/manifest.json",
+        tools: [],
+        options: {}
+      };
+
+    case queryParams.view === 'xml':
+      return { xmlDoc: "<xml>This is a sample XML view of the page.</xml>", "Content-Type": "application/xml" };
+
+    case queryParams.view === 'html':
+      return { htmlDoc: "<html><body><h1>This is a sample HTML view of the page.</h1></body></html>", "Content-Type": "text/html" };
+
+    case queryParams.embed === true:
+      return createPageResponse(id);
+
+    default:
+      return { ...createPageResponse(id), "Content-Type": "application/json" };
   }
-  else
-  {
-    return createPageResponse(id);    
-  }
+
 }
