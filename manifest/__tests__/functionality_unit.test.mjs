25d466c62419c6cfe5a4d7026c6328745fd7bedf
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-03-25 12:38:42 -0500

    correct functionality test for /manifest changes

--- manifest/__tests__/functionality_unit.test.mjs
+++ manifest/__tests__/functionality_unit.test.mjs
@@ -8,15 +8,30 @@
  * */
 
 import * as logic from '../manifest.mjs'
-import {validateID} from '../../utilities/shared.mjs'
+
+let test_manifest = { "type": "Manifest", "label": {"en":["Test Manifest"]} }
+let updated_manifest = {}
 
 describe('Manifest endpoint functionality unit test (just testing helper functions). #functions_unit', () => {
-  it('Need to redo tests to account for new db controller', () => {
+  it('Creates a Manifest', async () => {
+    test_manifest = await logic.createManifest(test_manifest)
+    expect(test_manifest["@id"]).toBeTruthy()
+  })
+  it('Finds the manifest by _id', async () => {
+    const found = await logic.findTheManifestByID(test_manifest["@id"].split("/").pop())
+    expect(found["@id"].split("/").pop()).toBe(test_manifest["@id"].split("/").pop())
+  })
+  it('Updates the Manifest', async () => {
+    test_manifest.updated = true
+    updated_manifest = await logic.updateManifest(test_manifest)
+    expect(updated_manifest["@id"]).toBeTruthy()
+    expect(updated_manifest["@id"]).not.toBe(test_manifest["@id"])
+  })
+  it('Reads for the Manifest', async () => {
+    const found = await logic.queryForManifests({"@id":updated_manifest["@id"]})
+    expect(found.length).toBe(1)
+  })
+  it('Deletes the Manifest Stub', async () => {
     expect(true).toBe(true)
   })
-
-  // await logic.createManifest(manifestJSON)
-  // await logic.updateManifest(manifestJSON)
-  // await logic.queryForManifest(manifestJSON)
-
 }) 
--- manifest/manifest.mjs
+++ manifest/manifest.mjs
@@ -5,7 +5,7 @@
  * https://github.com/thehabes 
  * 
  * */
-
+import fetch from 'node-fetch'
 import Database from "../database/polyController.mjs"
 import * as utils from "../utilities/shared.mjs"
 
@@ -30,9 +30,8 @@ export async function queryForManifests(manifestJSON){
 }
 
 export async function findTheManifestByID(hash_id){
-   // If this is a RERUM ID, we just need the const prefix + hash
-   const uri = process.env.RERUMIDPREFIX+hash_id
-   return {"@id":uri, "type":"Manifest"}
-   // TODO just fetch the RERUM URI
-   //return await fetch()
+   // Since this relates to a RERUM resource, we just need the IRI (const prefix + hash) and we can fetch it.
+   // No need to query through TinyPEN (database) for this
+   const manifestIRI = process.env.RERUMIDPREFIX+hash_id
+   return await fetch(manifestIRI).then(res => res.json()).catch(err => {return err})
 }
