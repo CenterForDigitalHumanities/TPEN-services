db6feaceddecd040f2dfa4604854c5f234557609
Sandeep Kumar Sutharapu <118848198+Sandeep-1-Kumar@users.noreply.github.com>
2024-04-09 22:42:05 -0500

    Update functionality_unit.test.mjs

--- page/__tests__/functionality_unit.test.mjs
+++ page/__tests__/functionality_unit.test.mjs
@@ -1,19 +1,27 @@
-import {findPageById} from '../page.mjs'
-import {validateID} from '../../utilities/shared.mjs'
+import * as utils from "../utilities/shared.mjs"
 
-// These test the pieces of functionality in the route that make it work.
-describe('Page endpoint functionality unit test (just testing helper functions). #functions_unit', () => {
-
-  it('No TPEN3 page id provided.  Page validation must be false.', () => {
-    expect(validateID()).toBe(false)
-  })
-  it('Detect TPEN3 page does not exist.  The query for a TPEN3 page must be null.', async () => {
-    const page = await findPageById(-111)
-    expect(page).toBe(null)
-  })
-  it('TPEN3 page does exist.  Finding the page results in the page JSON', async () => {
-    let page = await findPageById(123)
-    expect(page).not.toBe(null)
-  })
-
-})
+export async function findPageById(id = null) {
+    let page = null;
+  
+    if (!utils.validateID(id)) {
+      return page;
+    }
+  
+    const mockPause = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(null);
+      }, 1500);
+    });
+  
+    const pageArray = [
+      { id: 123, text: "welcome to the page" }
+    ];
+  
+    page = pageArray.find((page) => page.id === id) || null;
+  
+    if (page === null) {
+        page = await mockPause;
+    }
+  
+    return page;
+  }
