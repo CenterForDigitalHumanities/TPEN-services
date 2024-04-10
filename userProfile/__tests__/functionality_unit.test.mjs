2ba6a1fe78114234c629c91588fd62cbfb0bee89
Sandeep Naidu <sandeepsutharapu18896@gmail.com>
2024-04-09 20:29:34 -0500

    removed semicollans

--- userProfile/__tests__/functionality_unit.test.mjs
+++ userProfile/__tests__/functionality_unit.test.mjs
@@ -10,19 +10,19 @@ describe('userProfile endpoint functionality unit test (just testing helper func
   
   it('Throws error and handles if id is 111', async () => {
     try {
-      const res = await findUserById(111);
-      expect(true).toBe(false);
+      const res = await findUserById(111)
+      expect(true).toBe(false)
     } catch (error) {
-      expect(error.message).toBe('Internal Server Error');
+      expect(error.message).toBe('Internal Server Error')
     }
-  });
+  })
   
   it('Throws error and handles if id is 222', async () => {
     try {
-      const res = await findUserById(222);
-      expect(true).toBe(false);
+      const res = await findUserById(222)
+      expect(true).toBe(false)
     } catch (error) {
-      expect(error.message).toBe('Internal Server Error');
+      expect(error.message).toBe('Internal Server Error')
     }
   });
   
@@ -32,24 +32,24 @@ describe('userProfile endpoint functionality unit test (just testing helper func
   })
 
   it('TPEN3 user does exist.  Finding the user results in the user JSON', async () => {
-    const user = await findUserById(123);
-    expect(user).not.toBe(null);
-    expect(user.url).toBe('https://store.rerum.io/v1/id/123');
+    const user = await findUserById(123)
+    expect(user).not.toBe(null)
+    expect(user.url).toBe('https://store.rerum.io/v1/id/123')
   })
 
   it('TPEN3 user has correct number of projects', async () => {
-    const user = await findUserById(123);
-    expect(user.number_of_projects).toBe(2);
+    const user = await findUserById(123)
+    expect(user.number_of_projects).toBe(2)
   })
 
   it('TPEN3 user has correct number of public projects', async () => {
-    const user = await findUserById(123);
-    expect(user.public_projects.length).toBe(2);
+    const user = await findUserById(123)
+    expect(user.public_projects.length).toBe(2)
   })
 
   it('TPEN3 user has correct profile', async () => {
-    const user = await findUserById(123);
-    expect(user.profile).not.toBe(null);
+    const user = await findUserById(123)
+    expect(user.profile).not.toBe(null)
   })
 
 
