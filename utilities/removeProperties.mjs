89a8f9cfb2ee479b213fff502bbcf662fa288759
Onoja Victor <onoja.jsdev@gmail.com>
2024-04-15 11:07:04 -0500

    add utility functions to extract or exclude properties from object

--- utilities/removeProperties.mjs
+++ utilities/removeProperties.mjs
@@ -1,9 +1,36 @@
-export const removeProperties = (userObject, ...propertiesToRemove)=>{
- if(!userObject) return
- const { ...publicUser } = userObject;
+
+
+/**
+ * filters object 
+ * @param {*} obj the object to be filtered
+ * @param {*} propertiesToRemove the properties to be removed, e.g profile, password, links; array of strings
+ * @returns all properties of the except except one specified
+ */
+export const removeProperties = (obj, ...propertiesToRemove)=>{
+ if(!obj) return
+ const { ...modifiedObj } = obj;
  for (const property of propertiesToRemove) {
-     delete publicUser[property];
-   }
-   // publicUser.links = profile?.links //Assuming something in the profile object is public. NB profile has to be sourced from the destructure on line 3
- return publicUser
+     delete modifiedObj[property];
+    }
+    return modifiedObj
+   // publicUser.links = profile?.links //Assuming something in the profile object is public. Note that profile has to be sourced from the destructure as  const {profile, ...modifiedObj } = obj;
 } 
+
+
+/**
+ * 
+ * @param {*} obj object to be filtered
+ * @param {*} property the only property to return; string
+ * @returns object with one property
+ */
+export const includeOnly = (obj, property)=>{
+  let filteredObj = {}
+
+  for(key in obj){
+    if (key === property){
+      filteredObj[key] = obj[key]
+    }
+  }
+
+  return filteredObj
+}
