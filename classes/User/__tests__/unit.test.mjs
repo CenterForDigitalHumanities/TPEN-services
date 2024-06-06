2cb7d79c27f759ef23f8c3a761ef6ba7a8734e13
Onoja Victor <onoja.jsdev@gmail.com>
2024-06-05 22:43:55 -0500

    updated tests with mock data/methods

--- classes/User/__tests__/unit.test.mjs
+++ classes/User/__tests__/unit.test.mjs
@@ -1,43 +1,121 @@
-import {expect} from "@jest/globals"
+import {expect, jest} from "@jest/globals"
+import request from "supertest"
+import express from "express"
 import {User} from "../User.mjs"
 
-describe("User class constructor test #_unit #user_class", () => {
-  describe("constructor", () => {
-    it("should set the id property when provided an object", () => {
-      const user = new User({_id: "123"})
-      expect(user.id).toBe("123")
-    })
+import privateProfileRouter from "../../../userProfile/privateProfile.mjs"
+import auth0Middleware from "../../../auth/index.mjs"
 
-    it("should set the id property when provided a string", () => {
-      const user = new User("456")
-      expect(user.id).toBe("456")
-    })
+jest.mock("../User.mjs")
+jest.mock("../../../auth/index.mjs")
+
+const app = express()
+
+let token = process.env.TEST_TOKEN
+
+// app.use( (req, res, next)=>{
+//   req.auth = {
+//     payload: {
+//       "http://store.rerum.io/agent": "agent_value",
+//       _id: "123456"
+//     },
+//     token:`Bearer ${token}`
+//   };
+//   req.user = {
+//     _id: "123456",
+//     username: "exampleUser",
+//     profile: { name: "John Doe" }
+//   };
+
+//   next()
+
+// })
+
+app.use("/my", privateProfileRouter)
+
+describe("GET /my/profile #user_class", () => {
+  it("should return 200", async () => {
+    const response = await request(app)
+      .get("/my/profile")
+      .set("Authorization", `Bearer ${token}`)
+    expect(response.status).toBe(200)
   })
 })
 
-describe("User class methods #_unit #user_class", () => {
-  describe("getSelf and getUserById", () => {
-    it("should call getSelf and includeOnly with profile and _id", async () => {
-      const user = new User({_id: "660d801652df1c2243d6d935"})
-      const includedUser = await user.getUserById()
-      expect(includedUser).toEqual({
-        profile: expect.any(Object),
-        _id: "660d801652df1c2243d6d935"
-      })
-    })
+describe("GET /my/projects #user_class", () => {
+  it("should return 200", async () => {
+    const response = await request(app)
+      .get("/my/projects")
+      .set("Authorization", `Bearer ${token}`)
+    expect(response.status).toBe(200)
+  })
+})
 
-    it("should throw the error from getById", async () => {
-      const user = new User({_id: "123"})
-      const includedUser = await user.getUserById()
-      expect(includedUser).toMatchObject({})
+describe("GET /my/profile #user_class", () => {
+  beforeAll(() => {
+    jest.spyOn(User.prototype, "getSelf").mockResolvedValue({
+      _id: "123456",
+      userData: {name: "VOO"}
     })
   })
 
-  describe("getProjects", () => {
-    it("should call getSelf and includeOnly with profile and _id", async () => {
-      const user = new User({_id: "660d801652df1c2243d6d935"})
-      const userProjects = await user.getProjects()
-      expect(userProjects).toBeInstanceOf(Array)
-    })
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it("should return user profile data", async () => {
+    const response = await request(app)
+      .get("/my/profile")
+      .set("Authorization", `Bearer ${token}`)
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("userData")
+    expect(response.body.userData).toEqual({name: "VOO"})
+  })
+
+  it("should return 401 if user is not authenticated", async () => {
+    const appWithoutAuth = express()
+    appWithoutAuth.use("/my", privateProfileRouter)
+    const response = await request(appWithoutAuth).get("/my/profile")
+    expect(response.status).toBe(401)
+  })
+})
+
+describe("GET /my/projects #user_class", () => {
+  beforeAll(() => {
+    jest.spyOn(User.prototype, "getProjects").mockResolvedValue([
+      {
+        _id: "project.id",
+        "@type": "Project",
+        creator: "user.agent",
+        groups: {
+          members: [{agent: "user.agent", _id: "user._id"}]
+        }
+      }
+    ])
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it("should return user projects as an array", async () => {
+    const response = await request(app)
+      .get("/my/projects")
+      .set("Authorization", `Bearer ${token}`)
+    expect(response.status).toBe(200)
+    expect(Array.isArray(response.body)).toBe(true)
+
+    if (Array.isArray(response.body)) {
+      for (const obj of response.body) {
+        expect(obj).toHaveProperty("@type", "Project")
+      }
+    }
+  })
+
+  it("should return 401 if user is not authenticated", async () => {
+    const appWithoutAuth = express()
+    appWithoutAuth.use("/my", privateProfileRouter)
+    const response = await request(appWithoutAuth).get("/my/profile")
+    expect(response.status).toBe(401)
   })
 })
