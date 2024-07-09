24c483da86f2a4d17a4b305ecc1cb68caefa9153
Onoja Victor <onoja.jsdev@gmail.com>
2024-07-09 17:36:24 -0500

    cleanup, rewrite /:id

--- userProfile/index.mjs
+++ userProfile/index.mjs
@@ -1,59 +1,38 @@
-import express from 'express'
-import * as utils from '../utilities/shared.mjs'
-import * as service from './userProfile.mjs'
-import cors from 'cors'
-import common_cors from '../utilities/common_cors.json' assert {type: 'json'}
+import express from "express"
+import {respondWithError, validateID} from "../utilities/shared.mjs"
+import cors from "cors"
+import common_cors from "../utilities/common_cors.json" assert {type: "json"}
+import {User} from "../classes/User/User.mjs"
 
 let router = express.Router()
-router.use(
-  cors(common_cors)
-)
+router.use(cors(common_cors))
 
-router.route('/:id?')
-  .get(async (req, res, next) => {
-    let id = req.params.id
-    if (!id) {
-      utils.respondWithError(res, 400, 'No user ID provided')
-      return
-    }
-    if (!utils.validateID(id)) {
-      utils.respondWithError(res, 400, 'The TPEN3 user ID must be a number')
-      return
-    }
-    id = parseInt(id)
-    try{
-      const userObject = await service.findUserById(id)
-      if (userObject) {
-        respondWithUserProfile(res, userObject)
+router.route("/:id?").get(async (req, res) => {
+  let {id} = req.params 
+  // let id = "jkl"
+   if (!id) {
+    return respondWithError(res, 400, "No user ID provided")
+  }
+  
+  if (!validateID(id)) {
+    return respondWithError(res, 400, "The TPEN3 user ID is invalid")
+  }
+  const userObj = new User(id)
+  userObj
+    .getUserById()
+    .then((userData) => {
+      if(!Object.keys(userData).length){
+        return respondWithError(res, 200, `No TPEN3 user with ID '${id}' found`)
       }
-      else {
-        utils.respondWithError(res, 404, `TPEN3 user "${id}" does not exist.`)
-      }
-    }
-    catch (error) {
-      utils.respondWithError(res,500,error.message)
-    }  
-  })
-
-//post handler
-.post(async (req, res, next) => {
-  // open for future Modifications as needed
-  utils.respondWithError(res, 501, 'Not Implemented, please use GET.')
+      res.status(200).json(userData)
+    })
+    .catch((error) => {
+      respondWithError(
+        res,
+        error.status || error.code || 500,
+        error.message ?? "An error occurred while fetching the user data."
+      )
+    })
 })
-
-//put handler
-.put(async (req, res, next) => {
-  // open for future Modifications as needed
-  utils.respondWithError(res, 501, 'Not Implemented, please use GET.')
-})
-
-.all((req, res, next) => {
-  utils.respondWithError(res, 405, 'Improper request method, please use GET.')
-})
-
-function respondWithUserProfile(res, userObject) {
-  res.set('Content-Type', 'application/json; charset=utf-8')
-  res.status(200).json(userObject)
-}
 
 export default router
