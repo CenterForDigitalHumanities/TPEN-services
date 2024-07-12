50b1b7724b52c7ed6b0ae2c9717e64b6e8cb18fa
Onoja Victor <onoja.jsdev@gmail.com>
2024-07-12 09:30:10 -0500

    project cleanup -- Use Project class to create and get

--- classes/Project/Project.mjs
+++ classes/Project/Project.mjs
@@ -1,33 +1,36 @@
 import dbDriver from "../../database/driver.mjs"
-import { validateProjectPayload } from "../../utilities/validatePayload.mjs"
+import {validateProjectPayload} from "../../utilities/validatePayload.mjs"
 import {User} from "../User/User.mjs"
 
 let err_out = Object.assign(new Error(), {
   status: 500,
-  message: "Unknown Server error",
- })
+  message: "Unknown Server error"
+})
 
 const database = new dbDriver("mongo")
 
 export default class Project {
+  #creator
   constructor(userId) {
-    this.creator = userId
+    this.id = userId
   }
 
+  set creator(userAgent) {
+    this.#creator = userAgent
+  }
   async create(payload) {
     // validation checks for all the required elements without which a project cannot be created. modify validateProjectPayload function to include more elements as they become available (layers,... )
     const validation = validateProjectPayload(payload)
- 
+
     if (!validation.isValid) {
       err_out.status = 400
-      err_out.message =   validation.errors
+      err_out.message = validation.errors
       throw err_out
     }
 
     try {
-     return await database.save({...payload, "@type": "Project"})
-    
-    } catch (err) { 
+      return await database.save({...payload, "@type": "Project"})
+    } catch (err) {
       throw {
         status: err.status || 500,
         message: err.message || "An error occurred while creating the project"
@@ -45,25 +48,19 @@ export default class Project {
     return database.remove(projectId)
   }
 
-  async getProject(projectId) {
+  async getById(projectId) {
     if (!projectId) {
       err_out.message = "Project ID is required"
       err_out.status = 400
       throw err_out
     }
     return database
-      .getById(projectId)
-      .then((resp) => {
-        resp.json()
-      })
-      .catch((error) => {
-        console.log(error)
-        throw error
-      })
+      .getById(projectId, "Project")
+      .then((resp) => (resp?.length ? resp[0] : resp))
   }
 
   async getProjects() {
-    const userObj = new User(this.creator)
+    const userObj = new User(this.id)
     return await userObj.getProjects()
   }
 }
