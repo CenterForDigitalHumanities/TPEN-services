ab2009ff44ad8a43f6c7a8254eca1be89bb1800a
Patrick Cuba <cubap@slu.edu>
2024-09-19 10:31:57 -0500

    Update checkPermissions.mjs
    - :

--- project/groups/checkPermissions.mjs
+++ project/groups/checkPermissions.mjs
@@ -2,27 +2,27 @@ import Permissions from "./permissions.js";
 import { ACTIONS, ENTITIES, SCOPES } from "./permissions_parameters.mjs"
 
 const hasPermission = (role, action, scope, entity) => {
-  const rolePermissions = Permissions[role];
+  const rolePermissions = Permissions[role]
 
   if (!rolePermissions) return false; 
 
    if (rolePermissions[action]?.[scope]?.includes(entity)) {
-    return true;
+    return true
   }
  
   if (rolePermissions[ACTIONS.ALL]?.[SCOPES.ALL]?.includes(ENTITIES.ALL)) {
-    return true;
+    return true
   }
 
    if (rolePermissions[ACTIONS.ALL]?.[scope]?.includes(entity)) {
-    return true;
+    return true
   }
 
    if (rolePermissions[action]?.[SCOPES.ALL]?.includes(entity)) {
-    return true;
+    return true
   }
 
-  return false;  
-};
+  return false; 
+}
 
-export default hasPermission;
+export default hasPermission
