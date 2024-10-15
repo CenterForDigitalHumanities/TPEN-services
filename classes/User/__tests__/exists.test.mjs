5f9be2cc6ce910be937e70ec76eeb4bf1915f68c
Onoja <onoja.jsdev@gmail.com>
2024-10-15 11:09:29 -0500

    Add new project test cases - add member, remove member

--- classes/User/__tests__/exists.test.mjs
+++ classes/User/__tests__/exists.test.mjs
@@ -11,5 +11,8 @@ describe("user Class appears and behaves as expected #user_exists_test #user_cla
     expect(user.getSelf).toBeInstanceOf(Function)
     expect(user.updateRecord).toBeInstanceOf(Function) 
     expect(user.getProjects).toBeInstanceOf(Function) 
+    expect(user.getByEmail).toBeInstanceOf(Function) 
+    expect(user.getById).toBeInstanceOf(Function) 
+    expect(user.getByAgent).toBeInstanceOf(Function) 
   }) 
 })
