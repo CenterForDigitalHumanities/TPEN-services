adcead34f1cee78c0f75f70f679af68015732963
cubap <cubap@slu.edu>
2024-11-06 15:37:10 -0600

    another pipeline to prevent lots of calls

--- project/index.mjs
+++ project/index.mjs
@@ -23,7 +23,7 @@ router
     const projectObj = new Project()
 
     let project = req.body
-    project = { ...project, creator: user?.agent}
+    project = { ...project, creator: user?.agent }
 
     try {
       const newProject = await projectObj.create(project)
@@ -104,22 +104,11 @@ router
 
     (async () => {
       try {
-        const projectObj = await new Project(id)
-        const accessInfo = await projectObj.checkUserAccess(user._id, ACTIONS.READ, SCOPES.ALL, ENTITIES.PROJECT)
-        const project = await ProjectFactory.forInterface(projectObj.data)
- 
+        const project = await ProjectFactory.loadAsUser(id, user._id)
         if (!project) {
           return respondWithError(res, 404, `No TPEN3 project with ID '${id}' found`)
-        } else if (!accessInfo.hasAccess) {
-          return respondWithError(res, 401, accessInfo.message)
-        } 
-
-        if (accessInfo.hasAccess) {
-          res.status(200).json(project)
-        } else {
-          respondWithError(res, 403, accessInfo.message)
         }
-
+        res.status(200).json(project)
       } catch (error) {
         return respondWithError(
           res,
