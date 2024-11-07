e95b907df8f91df1aee9de7855dfcd21214594e5
cubap <cubap@slu.edu>
2024-11-07 10:52:27 -0600

    Update isDefaultRole.test.mjs

--- utilities/__tests__/isDefaultRole.test.mjs
+++ utilities/__tests__/isDefaultRole.test.mjs
@@ -38,16 +38,4 @@ describe('scrubDefaultRoles function #customRole_unit', () => {
         const result = scrubDefaultRoles(roles)
         expect(result).toBe(false)
     })
-
-    it('should convert string to array', () => {
-        const roles = 'admin'
-        const result = scrubDefaultRoles(roles)
-        expect(result).toBe(['admin'])
-    })
-
-    it('should split string by space', () => {
-        const roles = 'admin user'
-        const result = scrubDefaultRoles(roles)
-        expect(result).toEqual(['admin', 'user'])
-    })
 })
