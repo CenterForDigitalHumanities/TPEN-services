50586fa77f9942acc87eb797d5c0f2d0bc2ddd04
Patrick Cuba <cubap@slu.edu>
2025-03-07 13:58:20 -0600

    Bryan's refusal to .jsonld makes this not work as expected

--- classes/Manifest/__tests__/sample.js
+++ classes/Manifest/__tests__/sample.js
@@ -1,11 +1,11 @@
 import Manifest from '../Manifest.js'
 
-let m = new Manifest('https://static.t-pen.org/010101010101010101010101/manifest.json')
+let m = new Manifest('https://dms-data.stanford.edu/data/manifests/Parker/bc854fy5899/manifest.json')
 console.log(m.uri)
 console.log(m.manifest)
 m.load().then(manifest => console.log(manifest))
 // Output:
-// https://static.t-pen.org/010101010101010101010101/manifest.json
+// https://dms-data.stanford.edu/data/manifests/Parker/bc854fy5899/manifest.json
 // null
 // {
 //     "@context": "http://iiif.io/api/presentation/3/context.json",
