3f69eaecb2a50e633f48f39b65fac3a3c5c6dc75
cubap <cubap@slu.edu>
2025-04-07 13:50:46 -0500

    _sub is no longer missing on invitees

--- classes/Project/Project.mjs
+++ classes/Project/Project.mjs
@@ -123,6 +123,7 @@ export default class Project {
     const inviteCode = this.#generateInviteCode(user._id)
     const agent = `https://store.rerum.io/v1/id/${user._id}`
     const profile = { displayName: email.split("@")[0] }
+    const _sub = `temp-${user._id}` // This is a temporary sub for the user until they verify their email
     user.data = { email, profile, agent, inviteCode }
     await user.save()
     await this.inviteExistingTPENUser(user._id, roles)
