73112d38d03f3d3a59d952a10c51feac27c207bf
Patrick Cuba <cubap@slu.edu>
2025-04-14 16:30:04 -0500

    220 services for bug reporting and feedback (#224)
    * file dump init
    
    * services for feedback
    
    * /feedback/feedback is not a good route
    
    * Update cd_dev.yaml
    
    * stop if things are missing
    
    * adding verify on main
    
    * test results
    
    * added docs and ES6 conversion
    
    * what the Golden AI hell is adding imports from my docs?
    
    * explicitly adding CORS

--- feedback/feedbackRoutes.js
+++ feedback/feedbackRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import auth0Middleware from "../auth/index.mjs"
+import cors from 'cors'
 import { submitFeedback, submitBug } from './feedbackController.js'
+import common_cors from '../utilities/common_cors.json' with {type: 'json'}
 
 const feedbackRouter = express.Router()
-
+feedbackRouter.use(
+  cors(common_cors)
+)
 feedbackRouter.route('/feedback').post(auth0Middleware(), submitFeedback)
 feedbackRouter.route('/bug').post(auth0Middleware(), submitBug)
 
