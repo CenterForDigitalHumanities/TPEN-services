18b29baf5aafef6b17d249f552da86327871aa8f
cubap <cubap@slu.edu>
2025-04-28 15:08:18 -0500

    Update exists.test.mjs

--- classes/Page/__tests__/exists.test.mjs
+++ classes/Page/__tests__/exists.test.mjs
@@ -8,13 +8,14 @@ describe('Page Class looks how we expect it to. #Page_exists_unit', () => {
   const page = new Page("layerID", { id: "canvasID", label: "Canvas Label", target: "https://example.com/canvas" })
   it('has expected methods', () => {
     expect(typeof page.update).toBe('function')
-    expect(typeof page.save).toBe('function')
+    expect(typeof page.delete).toBe('function')
+    expect(typeof Page.build).toBe('function')
   })
 
   it('has expected properties', () => {
     expect(page).toHaveProperty('id')
     expect(page).toHaveProperty('label')
-    expect(page).toHaveProperty('url')
+    expect(page).toHaveProperty('target')
   })
 
   it('throws an error for poorly formed new Page calls', () => {
