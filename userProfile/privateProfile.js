20ce23cb2df4f98aae037d486fe10f5a9a334df9
Patrick Cuba <cubap@slu.edu>
2025-04-29 15:58:54 -0500

    Refactor all .mjs files to .js and update imports. Closes #194 (#228)
    * Update cd_dev.yaml
    
    * stop if things are missing
    
    * adding verify on main
    
    * Update cd_dev.yaml
    
    * stop if things are missing
    
    * adding verify on main
    
    * Update ci_dev.yaml (#219)
    
    * Update ci_dev.yaml
    
    * Update cd_prod.yaml
    
    * Update ci_prod.yaml
    
    * Update index.mjs
    
    * Update index.mjs
    
    * Update index.mjs
    
    * Update index.mjs
    
    * Refactor all .mjs files to .js and update imports. Closes #194
    
    * un-mjs
    
    * npm update
    
    * Hey I heard you like tests, so I put tests in your tests
    
    * mjs > js
    
    * no mjs, bad mjs
    
    * fine
    
    * habesroxx
    
    * how bout now
    
    * hide, Jest is coming
    
    * runner love
    
    * Update package-lock.json
    
    * jest no like to run
    
    * null != undefined
    
    * default not defaulting
    
    * fixes "id is not defined"
    
    shoulda wrote test for this
    
    * out of scope, out of effs
    
    ---------
    
    Co-authored-by: Priyal Patel <mepripri0712@gmail.com>

---
+++ userProfile/privateProfile.js
@@ -0,0 +1,50 @@
+import express from "express"
+import {respondWithError, respondWithJSON} from "../utilities/shared.js"
+import User from "../classes/User/User.js"
+import common_cors from '../utilities/common_cors.json' with {type: 'json'}
+import cors from "cors"
+import auth0Middleware from "../auth/index.js"
+
+const router = express.Router()
+router.use(
+  cors(common_cors)
+)
+
+router.route("/profile").get(auth0Middleware(), async (req, res) => {
+  const user = req.user
+  if (!user) return respondWithError(res, 401, "Unauthorized user")
+  try {
+    res.status(200).json(user)
+  } catch(error) {
+      respondWithError(res, error.status || error.code || 500, error.message?? "An error occurred while fetching the user data.")
+  }
+}).put(auth0Middleware(), async (req, res) => {
+  const user = req.user
+  if (!user) return respondWithError(res, 401, "Unauthorized user")
+  try {
+    const userObj = new User(user._id)
+    const userProfile = await userObj.updateProfile(req.body)
+    res.status(200).json(userProfile)
+  } catch (error) {
+    respondWithError(res, error.status || error.code || 500, error.message?? "An error occurred while fetching the user data.")
+  }
+}).all((req, res)=>    respondWithError(res, 405, "Improper request method. Use PUT instead"))
+
+router.route("/projects").get(auth0Middleware(), async (req, res) => {
+  const user = await req.user
+  if (!user) return respondWithError(res, 401, "Unauthorized user")
+   try {
+    const userObj = await new User(user._id)
+    const userProjects = await userObj.getProjects()
+
+    res.set("Content-Type", "application/json; charset=utf-8")
+
+    res.status(200).json(userProjects)
+  } catch (error) {
+    respondWithError(res, error?.status??500, error?.message??error.toString())
+  }
+}).all((req, res)=>    respondWithError(res, 405, "Improper request method. Use GET instead")
+)
+
+ 
+export default router
