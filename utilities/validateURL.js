ae1c06f2d67904029881e0761e9a62024b181c71
cubap <cubap@slu.edu>
2025-05-07 11:08:31 -0500

    Squashed commit of the following:
    commit 44b75cf175f43ef5b7736cf7f7f9754b1dcd7f36
    Author: Priyal Patel <52342511+mepripri@users.noreply.github.com>
    Date:   Wed May 7 10:13:21 2025 -0500
    
        Localhost URLS Validating (#242)
    
        * Update index.js
    
        * Update index.js
    
        * Update validateURL.js
    
        ---------
    
        Co-authored-by: Patrick Cuba <cubap@slu.edu>
    
    commit 032ef458dd9c0602ca3c23b795a39f0de2dd7d7d
    Merge: f977234 52edaab
    Author: cubap <cubap@slu.edu>
    Date:   Mon May 5 10:40:24 2025 -0500
    
        Merge branch 'development' into import-tpen28
    
    commit 52edaab21d47427f3003c94b94b848f64f04850c
    Author: Priyal Patel <52342511+mepripri@users.noreply.github.com>
    Date:   Fri May 2 09:36:27 2025 -0500
    
        Getting User Projects (#237)
    
        * Update index.mjs
    
        * Update index.mjs
    
        * Update index.mjs
    
        * Update index.mjs
    
        * Update index.mjs
    
        * Using UID to get User Projects
    
        * Update index.mjs
    
        * Origin Fetch
    
        * SetHeader Origin
    
        * Update index.js
    
        * Update index.js
    
        * Update index.js
    
    commit f97723414967e86eccda10f29ab02c4d89e7a7e9
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Thu May 1 13:14:16 2025 -0500
    
        Update index.js
    
    commit dde7c3015f32cb38d6c49cf25ac826a347310c26
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Thu May 1 13:05:02 2025 -0500
    
        SetHeader Origin
    
    commit d2ec19844b8a15fe4497eb877d6709f898487324
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Thu May 1 12:47:32 2025 -0500
    
        Origin Fetch
    
    commit 83fac3a3cf8bea72c5941b3e1cba7f95fe130d65
    Merge: 6e79a8d 2e5bbbd
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Thu May 1 09:31:12 2025 -0500
    
        Merge branch 'development' into import-tpen28
    
    commit 2e5bbbd0dd8d7690d9c7f6c33394fbac28d813dc
    Merge: 443d0a2 9f102fb
    Author: Patrick Cuba <cubap@slu.edu>
    Date:   Wed Apr 30 22:38:43 2025 -0500
    
        Merge branch 'development' of https://github.com/CenterForDigitalHumanities/TPEN-services into development
    
    commit 443d0a2d76c8eb5226fc9b7773b695c13d690943
    Author: cubap <cubap@slu.edu>
    Date:   Wed Apr 30 15:23:02 2025 -0500
    
        no label is fine for Pages
    
    commit 6e79a8d24aa70c8844859c5c70c1eb1766303d9d
    Merge: ac0182f 9f102fb
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Wed Apr 30 12:06:38 2025 -0500
    
        Merge branch 'development' into import-tpen28
    
    commit ac0182f62ae4ea53a32f4fb3d70baac41dc101ce
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Wed Apr 30 11:51:40 2025 -0500
    
        Update index.mjs
    
    commit 673a5c5c7f8b729c845e05eacbfade3e8f354906
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Tue Apr 29 17:22:08 2025 -0500
    
        Using UID to get User Projects
    
    commit afe664e776954a4689ae174ee7a40f69c5c5d7a6
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Mon Apr 28 10:16:27 2025 -0500
    
        Update index.mjs
    
    commit 6bf9c9a704df759f553a8f7fff86556108c4c0c2
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Fri Apr 25 14:56:11 2025 -0500
    
        Update index.mjs
    
    commit 0906084f2ebeaa966caf86999c3c2bc85300a314
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Fri Apr 25 14:50:54 2025 -0500
    
        Update index.mjs
    
    commit 5dd077e02ca0a4fddf9df52e959a519e7368769d
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Fri Apr 25 14:33:53 2025 -0500
    
        Update index.mjs
    
    commit e9971bc0bc892a9391f4918bffb45e65fddae550
    Author: Priyal Patel <mepripri0712@gmail.com>
    Date:   Fri Apr 25 12:18:30 2025 -0500
    
        Update index.mjs

--- utilities/validateURL.js
+++ utilities/validateURL.js
@@ -7,7 +7,7 @@ async function validateURL(url) {
   }
 
   const urlPattern = new RegExp(
-    "^(https?://([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]\\.)+[a-zA-Z]{2,}|(\\d{1,3}\\.){3}\\d{1,3})(:\\d+)?(/[-a-zA-Z0-9@:%_+.~#?&//=]*)?(\\?[;&a-zA-Z0-9@:%_+.~#?&//=]*)?(#[a-zA-Z0-9@:%_+.~#?&//=]*)?$"
+    "^(https?://((localhost)|(([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9].)+[a-zA-Z]{2,})|(\\d{1,3}\\.){3}\\d{1,3})(:\\d+)?(/[-a-zA-Z0-9@:%_+.~#?&//=]*)?(\\?[;&a-zA-Z0-9@:%_+.~#?&//=]*)?(#[a-zA-Z0-9@:%_+.~#?&//=]*)?)$"
   )
 
   if (!urlPattern.test(url)) {
