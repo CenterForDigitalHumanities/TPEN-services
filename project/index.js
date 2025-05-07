44b75cf175f43ef5b7736cf7f7f9754b1dcd7f36
Priyal Patel <52342511+mepripri@users.noreply.github.com>
2025-05-07 10:13:21 -0500

    Localhost URLS Validating (#242)
    * Update index.js
    
    * Update index.js
    
    * Update validateURL.js
    
    ---------
    
    Co-authored-by: Patrick Cuba <cubap@slu.edu>

--- project/index.js
+++ project/index.js
@@ -117,11 +117,6 @@ router
  
 router
   .route("/import28/:uid")
-  .options(async (req, res) => {
-      res.setHeader("Access-Control-Allow-Methods", req.get("origin"))
-      res.setHeader("Access-Control-Allow-Credentials", "true")
-      res.status(204).send()
-  })
   .get(cors(corsOptions), cookieParser(), patchTokenFromQuery, auth0Middleware(), async (req, res) => {
     const user = req.user
     const jsessionid = req.cookies.JSESSIONID
@@ -175,8 +170,6 @@ router
         return respondWithError(res, 500, "Invalid project response format")
       }
 
-      res.setHeader("Access-Control-Allow-Origin", req.get("origin"))
-      res.setHeader("Access-Control-Allow-Credentials", "true")
       return res.status(200).json({
         message: "Select a Project to Import : ",
         data: parsedData,
