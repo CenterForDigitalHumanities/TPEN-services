45255f77b51c718fd6638b9b38a16a72b5f4dfa9
cubap <cubap@slu.edu>
2025-05-16 10:26:28 -0500

    Update exists_unit.test.js

--- project/__tests__/exists_unit.test.js
+++ project/__tests__/exists_unit.test.js
@@ -7,75 +7,63 @@ const app = { _router: { stack: projectRouter.stack } }
 describe("Project endpoint availability unit test (via a check on the app routes)", () => {
   test("responds to /project/:id", () => {
     const stack = app._router.stack
-    // Looser check for :id route (Express regex is complex)
-    // Express compiles /project/:id to /project/([^/]+?)(?:/)?$/
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.get && middleware.regexp.toString().includes("/project/([^/]+?)"))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.get && middleware.regexp.toString().includes(":id/manifest"))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes("/create"))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes("/import"))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.get && middleware.regexp.toString().includes("/import28/"))).toBe(true)
+    // Use .test() with sample paths to check route regexes
+    expect(stack.some(mw => mw.route?.methods?.get && mw.regexp?.test('/project/123'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.get && mw.regexp?.test('/project/123/manifest'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/create'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/import'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.get && mw.regexp?.test('/project/import28/'))).toBe(true)
   })
 })
 
 describe("Hotkeys endpoint availability unit test (via a check on the app routes)", () => {
   test("responds to /project/:id/hotkeys", () => {
     const stack = app._router.stack
-    // Express compiles /project/:id/hotkeys to /project/([^/]+?)/hotkeys
-    const hotkeysPattern = "/project/([^/]+?)/hotkeys"
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.get && middleware.regexp.toString().includes(hotkeysPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.put && middleware.regexp.toString().includes(hotkeysPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.delete && middleware.regexp.toString().includes(hotkeysPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(hotkeysPattern))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.get && mw.regexp?.test('/project/123/hotkeys'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.put && mw.regexp?.test('/project/123/hotkeys'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.delete && mw.regexp?.test('/project/123/hotkeys'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/hotkeys'))).toBe(true)
   })
 })
 
 describe("Member and collaborator endpoint availability", () => {
   test("responds to /project/:id/invite-member and /project/:id/remove-member", () => {
     const stack = app._router.stack
-    const invitePattern = "/project/([^/]+?)/invite-member"
-    const removePattern = "/project/([^/]+?)/remove-member"
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(invitePattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(removePattern))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/invite-member'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/remove-member'))).toBe(true)
   })
   test("responds to /project/:projectId/collaborator/:collaboratorId/addRoles, setRoles, removeRoles", () => {
     const stack = app._router.stack
-    const addRolesPattern = "/project/([^/]+?)/collaborator/([^/]+?)/addRoles"
-    const setRolesPattern = "/project/([^/]+?)/collaborator/([^/]+?)/setRoles"
-    const removeRolesPattern = "/project/([^/]+?)/collaborator/([^/]+?)/removeRoles"
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(addRolesPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.put && middleware.regexp.toString().includes(setRolesPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(removeRolesPattern))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/collaborator/456/addRoles'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.put && mw.regexp?.test('/project/123/collaborator/456/setRoles'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/collaborator/456/removeRoles'))).toBe(true)
   })
   test("responds to /project/:projectId/switch/owner", () => {
     const stack = app._router.stack
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes("/switch/owner"))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/switch/owner'))).toBe(true)
   })
 })
 
 describe("Custom roles endpoint availability", () => {
   test("responds to /project/:projectId/addCustomRoles, setCustomRoles, removeCustomRoles", () => {
     const stack = app._router.stack
-    const addCustomRolesPattern = "/project/([^/]+?)/addCustomRoles"
-    const setCustomRolesPattern = "/project/([^/]+?)/setCustomRoles"
-    const removeCustomRolesPattern = "/project/([^/]+?)/removeCustomRoles"
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(addCustomRolesPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.put && middleware.regexp.toString().includes(setCustomRolesPattern))).toBe(true)
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.post && middleware.regexp.toString().includes(removeCustomRolesPattern))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/addCustomRoles'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.put && mw.regexp?.test('/project/123/setCustomRoles'))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.post && mw.regexp?.test('/project/123/removeCustomRoles'))).toBe(true)
   })
 })
 
 describe("Project metadata endpoint availability", () => {
   test("responds to /project/:projectId/metadata", () => {
     const stack = app._router.stack
-    const metadataPattern = "/project/([^/]+?)/metadata"
-    expect(stack.some(middleware => middleware.route && middleware.route.methods.put && middleware.regexp.toString().includes(metadataPattern))).toBe(true)
+    expect(stack.some(mw => mw.route?.methods?.put && mw.regexp?.test('/project/123/metadata'))).toBe(true)
   })
 })
 
 describe("Layer and page nested route availability", () => {
   test("responds to /project/:projectId/layer and /project/:projectId/page", () => {
     const stack = app._router.stack
-    expect(stack.some(middleware => middleware.name === 'router' && middleware.regexp.toString().includes("/layer"))).toBe(true)
-    expect(stack.some(middleware => middleware.name === 'router' && middleware.regexp.toString().includes("/page"))).toBe(true)
+    expect(stack.some(mw => mw.name === 'router' && mw.regexp?.test('/project/123/layer'))).toBe(true)
+    expect(stack.some(mw => mw.name === 'router' && mw.regexp?.test('/project/123/page'))).toBe(true)
   })
 })
