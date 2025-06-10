ae2de27f8b2a00f100a59644d8944905c29ffd40
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-06-10 13:12:44 -0500

    links

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function makeCleanFileFromMarkdown(file) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="http://localhost:4001/assets/css/main.css">
+      <link rel="stylesheet" href="https://three.t-pen.org/assets/css/main.css">
       <title>API Documentation</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
       <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
