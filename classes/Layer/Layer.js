c98a41c5ad5f8f32cac07c1f787430de05c391bd
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-07-23 09:15:33 -0500

    Layer and Page Management Endpoints (#278)
    * Fixes for updating Layers and Pages
    
    * Prepare PR
    
    * remove console.log()s
    
    * remove console.log()s
    
    * polish
    
    * polish
    
    * optimize
    
    * touchups from testing

--- classes/Layer/Layer.js
+++ classes/Layer/Layer.js
@@ -73,8 +73,8 @@ export default class Layer {
         return true
     }
 
-    async update() {
-        if (this.#tinyAction === 'update' || this.pages.some(page => page.id.startsWith(process.env.RERUMIDPREFIX))) {
+    async update(saveFirst=false) {
+        if (saveFirst) {
             this.#setRerumId()
             await this.#saveCollectionToRerum()
         }
