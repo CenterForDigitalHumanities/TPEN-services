d5c3ff8f1f3d6077237a8bd417c710dc06d6e9f3
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-07-30 13:24:43 -0500

    ok

--- bin/tpen3_services.js
+++ bin/tpen3_services.js
@@ -90,7 +90,7 @@ function onError(error) {
  */
 
 function onListening() {
-  console.log("LISTENING ON "+port)
+  console.log("BRYAN LISTENING ON "+port)
   const addr = server.address()
   const bind = typeof addr === 'string'
     ? 'pipe ' + addr
