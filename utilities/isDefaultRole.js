454b48482bb88757ee77ff8880582b89e0d02fa3
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-08-14 09:00:07 -0500

    changing variable name

--- utilities/isDefaultRole.js
+++ utilities/isDefaultRole.js
@@ -1,4 +1,4 @@
-const defaultRole = {
+const internalRoles = {
   OWNER: ["*_*_*"],
   LEADER: ["UPDATE_*_PROJECT", "READ_*_PROJECT", "*_*_MEMBER", "*_*_ROLE", "*_*_PERMISSION", "*_*_LAYER", "*_*_PAGE"],
   CONTRIBUTOR: ["READ_*_*", "UPDATE_TEXT_*", "UPDATE_ORDER_*", "UPDATE_SELECTOR_*", "CREATE_SELECTOR_*", "DELETE_*_LINE", "UPDATE_DESCRIPTION_LAYER", "CREATE_*_LAYER"],
@@ -6,7 +6,7 @@ const defaultRole = {
 }
 
 export default function scrubDefaultRoles(roleName) {
-  const defaultRoles = Object.keys(defaultRole)
+  const defaultRoles = Object.keys(internalRoles)
   if (Array.isArray(roleName)) {
     roleName = roleName.filter(roleString => {
       if (typeof roleString !== "string") throw new Error("Expecting a RolesMap and not an Array.")
