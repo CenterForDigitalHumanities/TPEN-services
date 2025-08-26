5beeb9950f510477069f3a0f0d483c424e829768
mepripri <52342511+mepripri@users.noreply.github.com>
2025-08-26 09:50:37 -0500

    API changed

--- userProfile/index.js
+++ userProfile/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { respondWithError, validateID } from "../utilities/shared.js"
+import { respondWithError } from "../utilities/shared.js"
 import cors from "cors"
 import common_cors from "../utilities/common_cors.json" with {type: "json"}
 import User from "../classes/User/User.js"
@@ -7,35 +7,17 @@ import User from "../classes/User/User.js"
 let router = express.Router()
 router.use(cors(common_cors))
 
-router.route("/:id?").get(async (req, res) => {
-  let { id } = req.params
-  // let id = "jkl"
-  if (!id) {
-    return respondWithError(res, 400, "No user ID provided")
+router.route("/:id").get(async (req, res) => {
+  const userId = req.params.id
+  if (!userId) return respondWithError(res, 400, "User ID is required")
+  try {
+    const userObj = new User(userId)
+    const publicProfile = await userObj.getPublicInfo()
+    if (!publicProfile) return respondWithError(res, 404, "User not found")
+    res.status(200).json(publicProfile)
+  } catch (error) {
+    respondWithError(res, error.status || error.code || 500, error.message ?? "An error occurred while fetching the user data.")
   }
-
-  if (!validateID(id)) {
-    return respondWithError(res, 400, "The TPEN3 user ID is invalid")
-  }
-
-  (async () => {
-
-    try {
-      const userObj = new User(id)
-      const publicUserInfo = await userObj.getPublicInfo()
-      if (publicUserInfo) {
-        res.status(200).json(publicUserInfo)
-      } else {
-        return respondWithError(res, 404, `No TPEN3 user with ID '${id}' found`)
-      }
-    } catch (error) {
-      respondWithError(
-        res,
-        error.status || error.code || 500,
-        error.message ?? "An error occurred while fetching the user data."
-      )
-    }
-  })()
-})
+}).all((req, res) => respondWithError(res, 405, "Improper request method. Use GET instead"))
 
 export default router
--- userProfile/privateProfile.js
+++ userProfile/privateProfile.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {respondWithError, respondWithJSON} from "../utilities/shared.js"
+import {respondWithError} from "../utilities/shared.js"
 import User from "../classes/User/User.js"
 import common_cors from '../utilities/common_cors.json' with {type: 'json'}
 import cors from "cors"
@@ -9,19 +9,6 @@ const router = express.Router()
 router.use(
   cors(common_cors)
 )
-
-router.route("/:id/public-profile").get(async (req, res) => {
-  const userId = req.params.id
-  if (!userId) return respondWithError(res, 400, "User ID is required")
-  try {
-    const userObj = new User(userId)
-    const publicProfile = await userObj.getPublicInfo()
-    if (!publicProfile) return respondWithError(res, 404, "User not found")
-    res.status(200).json(publicProfile)
-  } catch (error) {
-    respondWithError(res, error.status || error.code || 500, error.message ?? "An error occurred while fetching the user data.")
-  }
-}).all((req, res) => respondWithError(res, 405, "Improper request method. Use GET instead"))
 
 router.route("/profile").get(auth0Middleware(), async (req, res) => {
   const user = req.user
