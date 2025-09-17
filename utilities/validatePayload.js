f1430fe0da1f46c6a91e00966a0bb978348f5ace
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-09-17 12:42:56 -0500

    FIXME documentation

--- utilities/validatePayload.js
+++ utilities/validatePayload.js
@@ -3,6 +3,15 @@
  *
  * FIXME this is not validating the value type of the required keys, only that the keys themselves are present.
  * I can create bad projects with malformed data via the POST /project/create endpoint.
+ * Ex. this JSON will be validated even though it is severely malformed
+  {
+    "metadata": "metadata",
+    "label": "Some Label",
+    "layers": "layers",
+    "manifest": {"type":"Manifest"},
+    "tools": "tools",
+    "group": true
+  }
  *
  * @param payload - The JSON request body from the /project/create route.  
  */
