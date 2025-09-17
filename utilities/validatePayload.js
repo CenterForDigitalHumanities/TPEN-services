9d187dc9fff2ae33fc3d35e82d139f2d58a1122e
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-09-17 12:38:33 -0500

    FIXME documentation

--- utilities/validatePayload.js
+++ utilities/validatePayload.js
@@ -1,3 +1,11 @@
+/**
+ * Validate that the provided data payload is a valid Project object.
+ *
+ * FIXME this is not validating the value type of the required keys, only that the keys themselves are present.
+ * I can create bad projects with malformed data via the POST /project/create endpoint.
+ *
+ * @param payload - The JSON request body from the /project/create route.  
+ */
 export function validateProjectPayload(payload) {
   if (!payload) return { isValid: false, errors: "Project cannot be created from an empty object" }
   const requiredElements = ["metadata", "layers", "label", "manifest", "creator", "group"]
