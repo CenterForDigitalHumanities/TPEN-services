98be50efa695f8e9e9cbc613eaa929408c5e007a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-10-01 13:11:29 -0500

    polish

--- feedback/feedbackController.js
+++ feedback/feedbackController.js
@@ -12,15 +12,8 @@ import { isSuspiciousValueString } from "../utilities/checkIfSuspicious.js"
 export async function submitFeedback(req, res) {
   const user = req.user ? `${req.user.profile.displayName} (${req.user._id})` : 'Anonymous'
   const { page, feedback } = req.body
-
-  if (!feedback) {
-    return res.status(204).send()
-  }
-
-  if (isSuspiciousValueString(feedback)) {
-    return respondWithError(res, 400, "Suspicious input will not be processed.")
-  }
-
+  if (!feedback) return res.status(204).send()
+  if (isSuspiciousValueString(feedback)) return respondWithError(res, 400, "Suspicious input will not be processed.")
   try {
     await createGitHubIssue('Feedback', `Feedback from ${user}`, `Page: ${page}\n\nFeedback: ${sanitizeUserInput(feedback)}`)
     res.status(200).json({ message: 'Feedback submitted successfully' })
@@ -39,15 +32,8 @@ export async function submitFeedback(req, res) {
 export async function submitBug(req, res) {
   const user = req.user ? `${req.user.profile.displayName} (${req.user._id})` : 'Anonymous'
   const { page, bugDescription } = req.body
-
-  if (!bugDescription) {
-    return res.status(204).send()
-  }
-
-  if (isSuspiciousValueString(bugDescription)) {
-    return respondWithError(res, 400, "Suspicious input will not be processed.")
-  }
-
+  if (!bugDescription) return res.status(204).send()
+  if (isSuspiciousValueString(bugDescription)) return respondWithError(res, 400, "Suspicious input will not be processed.")
   try {
     await createGitHubIssue('Bug Report', `Bug reported by ${user}`, `Page: ${page}\n\nBug: ${sanitizeUserInput(bugDescription)}`)
     res.status(200).json({ message: 'Bug report submitted successfully' })
