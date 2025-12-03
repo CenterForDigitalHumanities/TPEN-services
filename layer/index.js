55e86a20e52c5d220a2b58208cf078098bd29437
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-12-03 09:00:44 -0600

    touchup

--- layer/index.js
+++ layer/index.js
@@ -69,9 +69,8 @@ router.route('/:layerId')
                   else layer[key] = null
                 }
             })
-            if (providedPages?.length === 0) providedPages = undefined
             let pages = []
-            if (providedPages && providedPages.length) {
+            if (Array.isArray(providedPages) && providedPages.length > 0) {
                 pages = await Promise.all(providedPages.map(p => findPageById(p.split("/").pop(), projectId) ))
                 layer.pages = pages
             }
